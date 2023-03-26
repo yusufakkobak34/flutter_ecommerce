1fe160a232cbc763c465aebd4ad2ea1daa07edba
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-03-27 02:18:03 +0300

    navbar package added.

--- lib/view/dashboard/dashboard_screen.dart
+++ lib/view/dashboard/dashboard_screen.dart
@@ -6,7 +6,35 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: IndexedStack(
+          index: 0,
+          children: [
+            Container(),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.orange,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(
+              color: Theme.of(context).colorScheme.secondary,
+              width: 1,
+            ),
+          ),
+        ),
+        
+      ),
     );
   }
 }
