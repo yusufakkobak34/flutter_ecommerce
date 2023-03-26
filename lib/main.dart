5d58f6940dd268c207a78c7d399bd091a1f06e1d
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-03-26 05:56:23 +0300

    theme configurations added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/route/app_page.dart';
 import 'package:flutter_ecommerce/route/app_route.dart';
+import 'package:flutter_ecommerce/theme/app_theme.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -16,6 +17,8 @@ class MyApp extends StatelessWidget {
       getPages: AppPage.list,
       initialRoute: AppRoute.dashboard,
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
