d726a64b35fd9eea86680ae57d2343f63b66c3bf
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-03-31 03:06:00 +0300

    section one completed.

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/component/main_header.dart';
 import 'package:flutter_ecommerce/controller/controllers.dart';
 import 'package:flutter_ecommerce/view/home/components/carousel_slider/carousel_slider_view.dart';
+import 'package:flutter_ecommerce/view/home/components/popular_category/popular_category_loading.dart';
 import 'package:get/get.dart';
 
 import 'components/carousel_slider/carousel_loading.dart';
@@ -20,6 +21,13 @@ class HomeScreen extends StatelessWidget {
               return CarouselSliderView(bannerList: homeController.bannerList);
             } else {
               return CarouselLoading();
+            }
+          }),
+          Obx(() {
+            if (homeController.popularCategoryList.isNotEmpty) {
+              return PopularCategoryLoading();
+            } else {
+              return PopularCategoryLoading();
             }
           })
         ],
