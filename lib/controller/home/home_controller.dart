fa309622f1b520ec61375e04ae7b5c5bc9bff7d5
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-02 03:38:54 +0300

    popular product services connection ok.

--- lib/controller/home/home_controller.dart
+++ lib/controller/home/home_controller.dart
@@ -1,21 +1,27 @@
 import 'package:flutter_ecommerce/model/ad_banner.dart';
 import 'package:flutter_ecommerce/model/category.dart';
+import 'package:flutter_ecommerce/model/product.dart';
 import 'package:flutter_ecommerce/service/remote_service/remote_banner_service.dart';
 import 'package:flutter_ecommerce/service/remote_service/remote_popular_category_service.dart';
 import 'package:get/get.dart';
+
+import '../../service/remote_service/remote_popular_product_service.dart';
 
 class HomeController extends GetxController {
   static HomeController instance = Get.find();
   RxList<AdBanner> bannerList = List<AdBanner>.empty(growable: true).obs;
   RxList<Category> popularCategoryList =
       List<Category>.empty(growable: true).obs;
+  RxList<Product> popularProductList = List<Product>.empty(growable: true).obs;
   RxBool isBannerLoading = false.obs;
   RxBool isPopularCategoryLoading = false.obs;
+  RxBool isPopularProductLoading = false.obs;
 
   @override
   void onInit() {
     getAdBanners();
     getPopularCategories();
+    getPopularProducts();
     super.onInit();
   }
 
@@ -39,7 +45,19 @@ class HomeController extends GetxController {
         popularCategoryList.assignAll(popularCategoryListFromJson(result.body));
       }
     } finally {
-      print(popularCategoryList.length);
+      isPopularCategoryLoading(false);
+    }
+  }
+
+  void getPopularProducts() async {
+    try {
+      isPopularProductLoading(true);
+      var result = await RemotePopularProductService().get();
+      if (result != null) {
+        popularProductList.assignAll(popularProductListFromJson(result.body));
+      }
+    } finally {
+      print(popularProductList.length);
       isPopularCategoryLoading(false);
     }
   }
