f84a850cd7297eb8fdccd7acb65210c120124096
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-02 05:21:33 +0300

    edits at popular product section.

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_ecommerce/controller/controllers.dart';
 import 'package:flutter_ecommerce/view/home/components/carousel_slider/carousel_slider_view.dart';
 import 'package:flutter_ecommerce/view/home/components/popular_category/popular_category.dart';
 import 'package:flutter_ecommerce/view/home/components/popular_category/popular_category_loading.dart';
+import 'package:flutter_ecommerce/view/home/components/popular_product/popular_product.dart';
 import 'package:flutter_ecommerce/view/home/components/popular_product/popular_product_loading.dart';
 import 'package:flutter_ecommerce/view/home/components/section_title.dart';
 import 'package:get/get.dart';
@@ -45,7 +46,8 @@ class HomeScreen extends StatelessWidget {
                   const SectionTitle(title: "Popüler Ürünler"),
                   Obx(() {
                     if (homeController.popularProductList.isNotEmpty) {
-                      return PopularProductLoading();
+                      return PopularProduct(
+                          popularProducts: homeController.popularProductList);
                     } else {
                       return PopularProductLoading();
                     }
