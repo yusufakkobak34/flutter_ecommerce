cf9f7c1f22d6626c9e69e3d3556722692f34ffcb
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-04 03:59:11 +0300

    fetch products backend completed.

--- lib/bindings/dashboard/dashboard_binding.dart
+++ lib/bindings/dashboard/dashboard_binding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_ecommerce/controller/dashboard/dashboard_controller.dart';
 import 'package:flutter_ecommerce/controller/home/home_controller.dart';
+import 'package:flutter_ecommerce/controller/product/product_controller.dart';
 import 'package:get/get.dart';
 
 class DashboardBinding extends Bindings {
@@ -7,5 +8,6 @@ class DashboardBinding extends Bindings {
   void dependencies() {
     Get.put(DashboardController());
     Get.put(HomeController());
+    Get.put(ProductController());
   }
 }
--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
+import 'package:flutter_ecommerce/model/tag.dart';
 import 'package:hive/hive.dart';
 
 part 'product.g.dart';
 
 List<Product> popularProductListFromJson(String val) =>
     List<Product>.from(jsonDecode(val)['data']
-        .map((category) => Product.popularProductListFromJson(category))
-        );
+        .map((val) => Product.popularProductListFromJson(val)));
+
+List<Product> productListFromJson(String val) =>
+    List<Product>.from(jsonDecode(val)['data']
+        .map((val) => Product.ProductFromJson(val)));        
 
 @HiveType(typeId: 3)
 class Product {
@@ -18,12 +22,15 @@ class Product {
   final String description;
   @HiveField(3)
   final List<String> images;
+  @HiveField(4)
+  final List<Tag> tags;
 
   Product({
     required this.id,
     required this.name,
     required this.description,
     required this.images,
+    required this.tags,
   });
 
   factory Product.popularProductListFromJson(Map<String, dynamic> data) =>
@@ -34,5 +41,17 @@ class Product {
               ['description'],
           images: List<String>.from(data['attributes']['product']['data']
                   ['attributes']['images']['data']
-              .map((image) => image['attributes']['url'])));
+              .map((image) => image['attributes']['url'])),
+          tags: []
+          );
+
+    factory Product.ProductFromJson(Map<String, dynamic> data) =>
+      Product(
+          id: data['id'],
+          name: data['attributes']['name'],
+          description: data['attributes']['description'],
+          images: List<String>.from(data['attributes']['images']['data']
+              .map((image) => image['attributes']['url'])),
+          tags: List<Tag>.from(data['attributes']['tags']['data'].map((val) => Tag.fromJson(val)))
+          );
 }
