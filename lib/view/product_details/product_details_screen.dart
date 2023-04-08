bab5c69ca652458a68775e5877ad0cc628445b53
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-09 02:18:34 +0300

    details screen completed,issue keyboard error.

--- lib/view/product_details/product_details_screen.dart
+++ lib/view/product_details/product_details_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/model/product.dart';
 import 'package:flutter_ecommerce/view/product_details/components/product_carousel_slider.dart';
+import 'package:intl/intl.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   final Product product;
   const ProductDetailsScreen({super.key, required this.product});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  NumberFormat formatter = NumberFormat('00');
+
+  int _qty = 1;
+  int _tagIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +24,15 @@ class ProductDetailsScreen extends StatelessWidget {
         child: Column(
           children: [
             ProductCarouselSlider(
-              images: product.images,
+              images: widget.product.images,
             ),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Text(
-                product.name,
+                widget.product.name,
                 style: TextStyle(
                   fontSize: 24,
                   color: Theme.of(context).primaryColor,
@@ -30,7 +43,7 @@ class ProductDetailsScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Text(
-               '\$${product.tags.first.price.toStringAsFixed(2)}',
+                '\$${widget.product.tags.first.price.toStringAsFixed(2)}',
                 style: TextStyle(
                   fontSize: 18,
                   color: Theme.of(context).primaryColor,
@@ -38,49 +51,157 @@ class ProductDetailsScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 20,),
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1
-                    ),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(8),
-                    ),
-                  ),
-                  child: Row(
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.keyboard_arrow_left_sharp,
-                          size: 32,
-                          color: Colors.grey.shade800,
-                          ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(8),
                       ),
-                      Text(
-                        '1',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey.shade800,
+                    ),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (_qty > 1) {
+                              setState(() {
+                                _qty--;
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_left_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
                         ),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.keyboard_arrow_right_sharp,
-                          size: 32,
-                          color: Colors.grey.shade800,
+                        Text(
+                          formatter.format(_qty),
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey.shade800,
                           ),
-                      ),
-                    ],
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              _qty++;
+                            });
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_right_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                )
-              ],
+                  const SizedBox(width: 10),
+                  //
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (_tagIndex > 0) {
+                              setState(() {
+                                _tagIndex--;
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_left_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        Text(
+                          widget.product.tags[_tagIndex].title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            if (_tagIndex != (widget.product.tags.length - 1)) {
+                              setState(() {
+                                _tagIndex++;
+                              });
+                            }
+                          },
+                          child: Icon(
+                            Icons.keyboard_arrow_right_sharp,
+                            size: 32,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text( 
+                'Bu ürün hakkında:',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                widget.product.description,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey.shade700,
+                ),
+              ),
             )
+
           ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            backgroundColor: MaterialStateProperty.all<Color>(
+              Theme.of(context).primaryColor,
+            ),
+          ),
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: const Text("Sepete Ekle",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+            ),
+          )
         ),
       ),
     );
