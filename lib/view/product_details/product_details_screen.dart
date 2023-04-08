33b2219bdae7bad40221ef04d9c9c77b9a76d5ec
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-09 01:45:31 +0300

    example product details screen carousel added.

--- lib/view/product_details/product_details_screen.dart
+++ lib/view/product_details/product_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/model/product.dart';
+import 'package:flutter_ecommerce/view/product_details/components/product_carousel_slider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   final Product product;
@@ -8,10 +9,79 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Breakpoint here. 
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            ProductCarouselSlider(
+              images: product.images,
+            ),
+            const SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.name,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+               '\$${product.tags.first.price.toStringAsFixed(2)}',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1
+                    ),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.keyboard_arrow_left_sharp,
+                          size: 32,
+                          color: Colors.grey.shade800,
+                          ),
+                      ),
+                      Text(
+                        '1',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey.shade800,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.keyboard_arrow_right_sharp,
+                          size: 32,
+                          color: Colors.grey.shade800,
+                          ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
