7c267b95f53f27a42b8b48ee85a3467930e5772c
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-04-12 03:50:44 +0300

    category controller's completed.

--- lib/controller/controllers.dart
+++ lib/controller/controllers.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_ecommerce/controller/category/category_controller.dart';
 import 'package:flutter_ecommerce/controller/dashboard/dashboard_controller.dart';
 import 'package:flutter_ecommerce/controller/home/home_controller.dart';
 import 'package:flutter_ecommerce/controller/product/product_controller.dart';
@@ -5,3 +6,4 @@ import 'package:flutter_ecommerce/controller/product/product_controller.dart';
 HomeController homeController = HomeController.instance;
 ProductController productController = ProductController.instance;
 DashboardController dashboardController = DashboardController.instance;
+CategoryController categoryController = CategoryController.instance;
