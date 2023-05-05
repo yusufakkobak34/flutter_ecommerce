fce788eb6386d28b21d874b49e31f29da72aa90a
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-05-06 01:31:23 +0300

    account page section's added.

--- lib/view/account/account_screen.dart
+++ lib/view/account/account_screen.dart
@@ -10,64 +10,99 @@ class AccountScreen extends StatelessWidget {
       child: ListView(
         physics: const BouncingScrollPhysics(),
         children: [
-        Row(
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.grey,
-              radius: 36,
-              child: CircleAvatar(
-                radius: 35,
-                backgroundImage: AssetImage("assets/user_image.png"),
-              ),
-            ),
-            const SizedBox(width: 10,),
-            Column(
-              children: [
-                Text("Hesabınıza giriş yapın",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 36,
+                child: CircleAvatar(
+                  radius: 35,
+                  backgroundImage: AssetImage("assets/user_image.png"),
                 ),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(height: 50),
-        buildAccount(),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Hesabınıza giriş yapın",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 50),
+          buildAccountCard(
+            title: "Profil Bilgisi",
+            onClick: () {},
+          ),
+          buildAccountCard(
+            title: "Bildirimler",
+            onClick: () {},
+          ),
+          buildAccountCard(
+            title: "Ayarlar",
+            onClick: () {},
+          ),
+          buildAccountCard(
+            title: "Hakkımızda",
+            onClick: () {},
+          ),
+          buildAccountCard(
+            title: "Hizmet Koşulları",
+            onClick: () {},
+          ),
+          buildAccountCard(
+            title: "Giriş Yap",
+            onClick: () {},
+          ),
         ],
       ),
     );
   }
 
-  Container buildAccount() {
-    return Container(
-        height: 60,
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.4),
-              spreadRadius: 0.1,
-              blurRadius: 7,
-            ),
-          ]
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Text(
-              "Profil Bilgisi",
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
+  Widget buildAccountCard(
+      {required String title, required Function() onClick}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 14),
+      child: GestureDetector(
+        onTap: () {
+          onClick();
+        },
+        child: Container(
+          height: 60,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(Radius.circular(12)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  spreadRadius: 0.1,
+                  blurRadius: 7,
+                ),
+              ]),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
               ),
-            ),
-            Icon(Icons.keyboard_arrow_left_outlined)
-          ],
+              const Icon(Icons.keyboard_arrow_right_outlined)
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
