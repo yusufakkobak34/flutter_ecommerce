cc4ec59ab950039e43978850fd6ae73d38251539
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-05-06 01:20:33 +0300

    build account method added.

--- lib/view/account/account_screen.dart
+++ lib/view/account/account_screen.dart
@@ -13,13 +13,61 @@ class AccountScreen extends StatelessWidget {
         Row(
           children: [
             CircleAvatar(
-              radius: 35,
-              backgroundImage: AssetImage("assets"),
-            )
+              backgroundColor: Colors.grey,
+              radius: 36,
+              child: CircleAvatar(
+                radius: 35,
+                backgroundImage: AssetImage("assets/user_image.png"),
+              ),
+            ),
+            const SizedBox(width: 10,),
+            Column(
+              children: [
+                Text("Hesabınıza giriş yapın",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
+        const SizedBox(height: 50),
+        buildAccount(),
         ],
       ),
     );
+  }
+
+  Container buildAccount() {
+    return Container(
+        height: 60,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 0.1,
+              blurRadius: 7,
+            ),
+          ]
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              "Profil Bilgisi",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+              ),
+            ),
+            Icon(Icons.keyboard_arrow_left_outlined)
+          ],
+        ),
+      );
   }
 }
