66c4222830e5efbd8c980ce6e68a7803373289cd
Yusuf Akkobak <yusufakkobak.work@gmail.com>
2023-05-10 20:56:04 +0300

    user model added.

---
+++ lib/model/user.dart
@@ -0,0 +1,13 @@
+class User {
+  String id;
+  String fullName;
+  String email;
+  String image;
+
+  User({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.image,
+  });
+}
